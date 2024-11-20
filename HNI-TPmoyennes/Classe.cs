1e43525bcef5589cd404aef9fd72a5eab6e482a3
romser99 <126883915+romser99@users.noreply.github.com>
2024-11-20 11:48:13 +0100

    Add files via upload

---
+++ HNI-TPmoyennes/Classe.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSE_TPmoyennes
+{
+    class Classe
+    {
+        public string nomClasse { get; private set; }
+        public List<Eleve> eleves { get; private set; }
+        public List<string> matieres { get; private set; }
+
+        public Classe(string nom)
+        {
+            nomClasse = nom;
+            eleves = new List<Eleve>();
+            matieres = new List<string>();
+        }
+
+        public void ajouterEleve(string prenom, string nom)
+        {
+            if (eleves.Count < 30)
+                eleves.Add(new Eleve(prenom, nom));
+        }
+
+        public void ajouterMatiere(string nomMatiere)
+        {
+            if (matieres.Count < 10)
+                matieres.Add(nomMatiere);
+        }
+
+        public float moyenneMatiere(int indexMatiere)
+        {
+            if (indexMatiere < 0 || indexMatiere >= matieres.Count)
+                return 0.0f;
+
+            float somme = 0;
+            int count = 0;
+            foreach (var eleve in eleves)
+            {
+                float moyenne = eleve.moyenneMatiere(indexMatiere);
+                if (moyenne > 0)
+                {
+                    somme += moyenne;
+                    count++;
+                }
+            }
+            return count > 0 ? (float)Math.Truncate(somme / count * 100) / 100 : 0.0f;
+        }
+
+        public float moyenneGeneral()
+        {
+            float somme = 0;
+            foreach (var matiere in matieres)
+            {
+                int index = matieres.IndexOf(matiere);
+                somme += moyenneMatiere(index);
+            }
+            return matieres.Count > 0 ? (float)Math.Truncate(somme / matieres.Count * 100) / 100 : 0.0f;
+        }
+    }
+}
---
+++ HNI-TPmoyennes/Eleve.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSE_TPmoyennes
+{
+    class Eleve
+    {
+        public string prenom { get; private set; }
+        public string nom { get; private set; }
+        private List<Note> notes;
+
+        public Eleve(string prenom, string nom)
+        {
+            this.prenom = prenom;
+            this.nom = nom;
+            notes = new List<Note>();
+        }
+
+        public void ajouterNote(Note note)
+        {
+            if (notes.Count < 200)
+                notes.Add(note);
+        }
+
+        public float moyenneMatiere(int indexMatiere)
+        {
+            float somme = 0;
+            int count = 0;
+            foreach (var note in notes)
+            {
+                if (note.matiere == indexMatiere)
+                {
+                    somme += note.note;
+                    count++;
+                }
+            }
+            return count > 0 ? (float)Math.Truncate(somme / count * 100) / 100 : 0.0f;
+        }
+
+        public float moyenneGeneral()
+        {
+            Dictionary<int, float> moyennesMatieres = new Dictionary<int, float>();
+            foreach (var note in notes)
+            {
+                if (!moyennesMatieres.ContainsKey(note.matiere))
+                    moyennesMatieres[note.matiere] = moyenneMatiere(note.matiere);
+            }
+            float somme = 0;
+            foreach (var moyenne in moyennesMatieres.Values)
+            {
+                somme += moyenne;
+            }
+            return moyennesMatieres.Count > 0 ? (float)Math.Truncate(somme / moyennesMatieres.Count * 100) / 100 : 0.0f;
+        }
+    }
+}
